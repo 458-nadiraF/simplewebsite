576f26e88f15cd23199aa1d29a3612d1cf01c37b
Nadira Fawziyya Masnur <68457610+458-nadiraF@users.noreply.github.com>
2026-07-31 06:06:01 +0700

    Create footer.js

---
+++ app/components/footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="footer" id="contact">
+      <div className="container">
+        <p>© {new Date().getFullYear()} Simple Site. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
