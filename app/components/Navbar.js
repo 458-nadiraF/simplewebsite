abd4db9700a5f6c612e1142193b5c3246cbc6b0b
Nadira Fawziyya Masnur <68457610+458-nadiraF@users.noreply.github.com>
2026-07-31 06:06:19 +0700

    Add files via upload

---
+++ app/components/Navbar.js
@@ -0,0 +1,17 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <span className="logo">Simple Site</span>
+        <ul className="nav-links">
+          <li>
+            <a href="#features">Features</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
